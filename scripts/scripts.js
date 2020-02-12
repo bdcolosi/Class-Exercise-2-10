04afa9cc7401d690a16efe5190b7e897e831b3b5
bdcolosi <bdcolosi27@gmail.com>
2020-02-11 22:20:54 -0500

    added modal and image

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,30 +1,62 @@
 "use strict";
 let category = "dev";
 
-// const refreshQuoteButton = document.querySelector('#refreshQuote');
-
+const refreshQuoteButton = document.querySelector('#refreshQuote');
 const sumbitFormButton = document.querySelector("#submitForm");
+const categoryChangeForm = document.querySelector('#categoryChangeForm');
+const closeModalButton = document.querySelector("#closeModal");
 
 function getQuote (category) {
     const apiUrl = `https://api.chucknorris.io/jokes/random?category=${category}`;
-    const chuckSaysParagraph = document.querySelector("#chuckSays");
+    const chuckSaysParagraph = document.querySelector('#chuckSays');
+    const modalWindow = document.querySelector(".modal-overlay");
+
+  get(apiUrl).then(function(response) {
+    chuckSaysParagraph.innerHTML = response.value;
+    modalWindow.classList.toggle("open");
+  });
+}
+ 
+
+function getCategories(){
+    const apiUrl = `https://api.chucknorris.io/jokes/categories`;
+    const categorySelectLabel = document.querySelector('#categorySelectLabel');
     
     get(apiUrl).then(function(response){
-        chuckSaysParagraph.innerHTML = response.value;
-    });
-}; 
+        const categoryList = response.filter(function(category){
+            if (category != 'explicit'){
+                return category
+            }
+        });
 
-// refreshQuoteButton.addEventListener('click', function(e){
-//     e.preventDefault();
-//     getQuote(category);
-// });
+        const categoryElement = document.createElement('select');
+
+        categoryList.map(function(category){
+            const categoryOption = document.createElement('option');
+            categoryOption.value = category;
+            categoryOption.text = category;
+            categoryElement.append(categoryOption); 
+        });
+        categorySelectLabel.appendChild(categoryElement);
+    });
+}
+
+refreshQuoteButton.addEventListener('click', function(e){
+    e.preventDefault();
+    getQuote(category);
+});
 
 sumbitFormButton.addEventListener('click', function(e){
     e.preventDefault();
-    // const getcategoryInput = document.querySelector("#categoryInput");
-    const userCategory = document.querySelector('#userCategorySelect');
-    category = userCategory.value;
+    const categoryInput = document.querySelector('#categoryChangeForm select');
+    category = categoryInput.value;
     getQuote(category);
 
 });
+
+closeModalButton.addEventListener('click', function(e){
+    const modalWindow = document.querySelector(".modal-overlay");
+    modalWindow.classList.toggle("open");
+})
 getQuote(category);
+getCategories();
